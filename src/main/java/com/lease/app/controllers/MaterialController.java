11cd17bc3f310cf6faf44ddf5414553e027ff79b
ashwin852 <rmaswin0311@gmail.com>
2024-02-23 16:40:21 +0530

    Material Api's Created

--- src/main/java/com/lease/app/controllers/MaterialController.java
+++ src/main/java/com/lease/app/controllers/MaterialController.java
@@ -3,12 +3,15 @@ package com.lease.app.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,7 +54,7 @@ public class MaterialController {
 			if(material.isPresent()) {
 				return new ResponseEntity<>(material.get(), HttpStatus.OK);
 			}else {
-				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+				return new ResponseEntity<>(null,HttpStatus.NOT_FOUND);
 			}
 		}catch(Exception e) {
 			//Need to log the error
@@ -59,26 +62,52 @@ public class MaterialController {
 		}
 	}
 	
+	@PostMapping("/create")
+	public ResponseEntity<Material> createMaterial(@RequestBody Material material){
+		try {
+			Material newMaterial = materialRepository
+									.save(new Material(material.getName(), material.getPricePerDay(), material.getQuantity() ,material.getRemaining()));
+			return new ResponseEntity<>(newMaterial, HttpStatus.CREATED);
+		} catch(Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<Material> updateMaterial(@PathVariable("id") long id, @RequestBody Material material){
+		
+		Optional<Material> retrievedMaterial = materialRepository.findById(id);
+		
+		if(retrievedMaterial.isPresent()) {
+			Material _material = retrievedMaterial.get();
+			_material.setName(material.getName());
+			_material.setPricePerDay(material.getPricePerDay());
+			_material.setQuantity(material.getQuantity());
+			_material.setRemaining(material.getRemaining());	
+			
+			return new ResponseEntity<>(materialRepository.save(_material), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+		}
+	}
 	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<HttpStatus> deleteMaterial(@PathVariable("id") long id){
+		try {
+			materialRepository.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@DeleteMapping("/delete/all")
+	public ResponseEntity<HttpStatus> deleteAllMaterials(){
+		try {
+			materialRepository.deleteAll();
+			return new ResponseEntity<>(HttpStatus.OK);
+		}catch(Exception e) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }
